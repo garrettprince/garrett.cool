891d058ee23d399196bb50119df311b00ac96a00
your_github_username <garrettjamesprince@gmail.com>
2024-08-13 21:26:59 -0700

    Trying border styling

--- src/components/2D/Card.js
+++ src/components/2D/Card.js
@@ -2,6 +2,8 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import React from "react";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
 
 export default function Card({
   title,
@@ -14,7 +16,7 @@ export default function Card({
   return (
     <Link
       href={link}
-      className="my-4 mr-3 p-6 w-fit min-w-[18rem] max-w-[18rem] h-[28rem] relative overflow-hidden rounded-xl group"
+      className="my-3 mr-3 p-6 w-fit min-w-[18rem] max-w-[18rem] h-[28rem] relative overflow-hidden rounded-xl group border border-black"
     >
       {video === true ? (
         <video
@@ -35,12 +37,31 @@ export default function Card({
       )}
 
       <div
-        className={`flex flex-col justify-end h-full ${
+        className={`flex flex-col justify-end h-full space-y-2 ${
           textBlack ? "text-black" : "text-white"
         }`}
       >
-        <p className="text-2xl font-medium mb-1">{title}</p>
-        <p className="text-sm">{description}</p>
+        <div>
+          <p className="text-2xl font-medium mb-">{title}</p>
+          <p className="text-sm">{description}</p>
+        </div>
+        <div
+          className={`flex items-center justify-between ${
+            textBlack ? "text-black" : "text-white"
+          }`}
+        >
+          <div className="h-6 bg-white border-black border rounded-md text-center text-sm pt-[0.2rem] px-2">
+            TUTORIAL
+          </div>
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            whileHover={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3 }}
+            className="group-hover:opacity-100 group-hover:translate-x-0 opacity-0 -translate-x-5 transition-all duration-300"
+          >
+            <ArrowRightIcon className="w-5 h-5" />
+          </motion.div>
+        </div>
       </div>
     </Link>
   );
